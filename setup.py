a3722ba7c49752eb18c4f0989a2a1b986ec96dbd
Adrian Karwat <adr.karwat@gmail.com>
2023-12-08 20:50:32 +0100

    changes in setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
- 
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='Alfred',
+    version='1.0.0',
+    description='CLI Bot assistant. Package for adding data to'
+                'address book, read/update/delete it, adding notes etc.',
+    url='https://github.com/rafalradx/alfred-assist-bot',
+    author1='Gotham Devs',
+    readme="README.md",
+    license="MIT",
+    packages=find_namespace_packages(),
+    #install_requires=[], #tą sekcję możemy zostawić pustą lub ją usunąć - do decyzji
+    entry_points={'console_scripts': ["Alfred=classes.run_alfred:main"]}
+    ) 
